7883152cdb31fc1a4133e473e8433b3aaa0cba31
Amara Sai Anitej <112148159+anitejamara@users.noreply.github.com>
2024-04-24 21:55:40 +0530

    Update prodMgmt.py

--- products/prodMgmt.py
+++ products/prodMgmt.py
@@ -161,4 +161,4 @@ def view_products():
         return jsonify({'message': f'Error: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
