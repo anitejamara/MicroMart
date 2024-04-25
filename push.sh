f4369f6212adeb33c75d9c6bd4d8ee31fcd860b6
Ashvin Bhat <ashvinbhat14@gmail.com>
2024-04-25 16:09:39 +0530

    v2.1 - This is a exmple commit
    changes since last commit :
    - fixed few requirement bugs
    - added kubernetes yml files and jenkins pipeline
    
    near term features pending :
    - get .env file and test kubernetes
    - test jenkins pipeline
    
    long term features pending :
    - none
    
    current commit policy - commit to main

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -t order-management ./orders/
-docker build -t product-management ./products/
-docker build -t user-management ./users/
+docker build -t order-management ./MicroMart/orders/
+docker build -t product-management ./MicroMart/products/
+docker build -t user-management ./MicroMart/users/
 docker tag order-management ashvinbhat/order-management:latest
 docker push ashvinbhat/order-management:latest
 docker tag product-management ashvinbhat/product-management:latest
@@ -11,4 +11,4 @@ docker tag user-management ashvinbhat/user-management:latest
 docker push ashvinbhat/user-management:latest
 kubectl delete services --all
 kubectl delete deployments --all
-kubectl apply -f ./Kubernetes/
+kubectl apply -f ./MicroMart/Kubernetes/
